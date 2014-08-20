bc5bc1de6eb55a07b513a3a82338efaf1f0d9a02
Rukiza <theproros@gmail.com>
2014-08-20 14:46:46 +1200

    Constructors added so they call the super constructers, and take a stratagy.

--- Video Player/src/videoplayer/layout/ButtonLayout.java
+++ Video Player/src/videoplayer/layout/ButtonLayout.java
@@ -6,9 +6,29 @@ import java.awt.GridLayout;
 
 
 public class ButtonLayout extends GridLayout{
+	
+	//TODO generate serial id when class is done.
+
 	private ButtonLayoutStratagy layoutStratagy;
 	
-	ButtonLayoutStratagy stratagy;
+	
+	//======================Constructors========================//
+	public ButtonLayout(ButtonLayoutStratagy layoutStratagy){
+		super();
+		this.layoutStratagy = layoutStratagy;
+	}
+	
+	public ButtonLayout(int rows, int cols, ButtonLayoutStratagy layoutStratagy){
+		super(rows, cols);
+		this.layoutStratagy = layoutStratagy;
+	}
+	
+	public ButtonLayout(int rows, int cols, int hgap, int vgap, ButtonLayoutStratagy layoutStratagy){
+		super(rows, cols, hgap, vgap);
+		this.layoutStratagy = layoutStratagy;
+	}
+	//=======================Constructors end===================//
+	
 	
 	@Override
 	public void layoutContainer(Container parent){
