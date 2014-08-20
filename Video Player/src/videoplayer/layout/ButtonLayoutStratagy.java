c44be84d99d08f44037986e097d1af4e30ab731c
Rukiza <theproros@gmail.com>
2014-08-20 14:48:50 +1200

    Interface change so that it implements the method with added promaters from Buttons layout that needs to change depending on the stratagy.

--- Video Player/src/videoplayer/layout/ButtonLayoutStratagy.java
+++ Video Player/src/videoplayer/layout/ButtonLayoutStratagy.java
@@ -1,5 +1,9 @@
 package videoplayer.layout;
 
+import java.awt.Container;
+
 public interface ButtonLayoutStratagy {
+	
+	public void layoutContainer(Container parent, ButtonLayout buttonLayout);
 	
 }
