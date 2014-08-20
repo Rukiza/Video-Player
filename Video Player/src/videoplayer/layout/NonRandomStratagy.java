014c34e037801ba0ba933d9900e746e442b0229b
Rukiza <theproros@gmail.com>
2014-08-20 14:49:23 +1200

    Overrides the interface method

--- Video Player/src/videoplayer/layout/NonRandomStratagy.java
+++ Video Player/src/videoplayer/layout/NonRandomStratagy.java
@@ -1,5 +1,13 @@
 package videoplayer.layout;
 
+import java.awt.Container;
+
 public class NonRandomStratagy implements ButtonLayoutStratagy {
+
+	@Override
+	public void layoutContainer(Container parent, ButtonLayout buttonLayout) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
