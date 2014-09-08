1aef2a377f2bc14c71fe449772d2123f227e1d0d
Rukiza <theproros@gmail.com>
2014-09-08 12:20:10 +1200

    Added some temp test code to main, OuterFrame had some code added to it to set its size also name change might be ablicable.

--- Video Player/src/videoplayer/Main.java
+++ Video Player/src/videoplayer/Main.java
@@ -1,5 +1,11 @@
 package videoplayer;
 
-public class Main {
+import videoplayer.gui.OuterFrame;
 
+public class Main {
+	public static void main(String arg[]){
+		OuterFrame frame = new OuterFrame();
+		frame.setSize(200, 200);
+		frame.setVisible(true);
+	}
 }
--- Video Player/src/videoplayer/gui/OuterFrame.java
+++ Video Player/src/videoplayer/gui/OuterFrame.java
@@ -1,9 +1,14 @@
 package videoplayer.gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 public class OuterFrame extends JFrame{
 	
-	
+	public OuterFrame(){
+		MenuBar menu = new MenuBar();
+		this.add(menu, BorderLayout.NORTH);
+	}
 	
 }
